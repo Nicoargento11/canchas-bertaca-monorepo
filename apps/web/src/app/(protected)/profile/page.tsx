9d9dcb11b4d77b62fd17f7d75f44f31751868dd7
Nicolas Valdes <valdes.nicoedu@gmail.com>
2025-12-12 20:17:11 -0300

    refactor(profile): simplify ProfilePage component and improve session handling

--- apps/web/src/app/(protected)/profile/page.tsx
+++ apps/web/src/app/(protected)/profile/page.tsx
@@ -1,29 +1,26 @@
-import Profile from "../_components/profile/profile";
-import { TooltipProvider } from "@/components/ui/tooltip";
+import PlayerProfile from "../_components/profile/PlayerProfile";
 import { getSession } from "@/services/auth/session";
 import { getComplexBySlug } from "@/services/complex/complex";
 import { getUserById } from "@/services/user/user";
 import { notFound } from "next/navigation";
 
-export default async function ProfilePage({ params }: { params: Promise<{ slug: string }> }) {
-  // const { slug } = await params;
-
+export default async function ProfilePage() {
   const sessionUser = await getSession();
-  const { data: complejo } = await getComplexBySlug("bertaca");
+
+  if (!sessionUser?.user.complexSlug) {
+    return notFound();
+  }
+
+  const { data: complejo } = await getComplexBySlug(sessionUser.user.complexSlug);
 
   if (!complejo) {
     return notFound();
   }
+
   const { success, data, error } = await getUserById(sessionUser?.user.id!);
   if (!success || !data) {
     return <div>Error</div>;
   }
 
-  return (
-    <div className="min-h-screen bg-gradient-to-br from-Primary-darker via-Primary to-Primary-light">
-      <TooltipProvider>
-        <Profile userData={data} slug={"bertaca"} />
-      </TooltipProvider>
-    </div>
-  );
+  return <PlayerProfile userData={data} slug={sessionUser.user.complexSlug} />;
 }
